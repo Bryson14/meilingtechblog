4c93729ba994a5e238562c404370aaf2249035ff
Bryson <bryson.meiling@gmail.com>
2023-12-28 13:56:46 -0500

    got mdx to work

--- README.md
+++ README.md
@@ -25,3 +25,6 @@ This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-opti
 - [ ] Get website up and running on amplify
 - [ ] Add MDX support
 - [ ] Set globals CSS and find a design
+- [ ] Sticky Footer like [this](https://mantine.dev/others/nprogress/)
+- [ ] Add dark mode
+- [ ] Add use system color scheme
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,29 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import React from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'New Gen Tech with Bryson',
-  description: 'A technology blog for learners and developers',
-}
+  title: "New Gen Tech with Bryson",
+  description: "A technology blog for learners and developers",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
